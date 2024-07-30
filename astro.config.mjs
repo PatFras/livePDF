6fad5bae3bc1434ea43e7acbc31adc8d0b530e12
Patricio Frascaroli <pmfrascaroli@gmail.com>
2024-07-30 18:31:10 -0300

    delete rewrites

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  output: "server",
+  output: "static",
   build: {
     serverEntry: "functions/server.js",
     server: "./functions",
