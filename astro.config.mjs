75b42d31b29ef294d22f9ba14e8c548e72cc55bf
Patricio Frascaroli <pmfrascaroli@gmail.com>
2024-07-31 12:02:55 -0300

    astro config integrations & adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: "server",
